70eb0aed7469acc9fdb7c12a07f474a0ae27c373
ezekiel55 <alegremato@gmail.com>
2024-03-06 09:32:56 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Example for working with remote repositories");
+
+        System.out.println("new functionality"); //this seems to do something
     }
     //
 }
